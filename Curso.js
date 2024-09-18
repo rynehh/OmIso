162d1a0f262c1f76d0b8b93c3699c9a03636d060
rynehh <ryneh_h@outlook.com>
2024-09-18 00:10:04 -0600

    Ventana de Curso
    Ventana de Curso (Sin datos Dummy)

---
+++ Curso.js
@@ -0,0 +1,8 @@
+function toggleContent(id) {
+    const content = document.getElementById(id);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
